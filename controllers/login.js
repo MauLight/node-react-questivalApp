eca804bada6ddcd0b8173964cb04260edd685ae7
MauLight <maulisseluz@gmail.com>
2023-12-08 23:28:40 -0300

    feat: :bug: fix in logged user
    provide info needed in front end

--- controllers/login.js
+++ controllers/login.js
@@ -13,7 +13,7 @@ loginRouter.post('/', async (request, response) => {
   //! Use compare function to check if password is correct
   const passwordCorrect = user === null ? false : await bcrypt.compare(password, user.passwordHash)
 
-  if(!(user && passwordCorrect)) {
+  if (!(user && passwordCorrect)) {
     return response.status(401).json({
       error: 'Invalid username or password'
     })
@@ -25,7 +25,16 @@ loginRouter.post('/', async (request, response) => {
   }
 
   const token = jwt.sign(userForToken, SECRET)
-  response.status(200).send({ token, email: user.email, firstname: user.firstname, lastname: user.lastname, id: user.id })
+  response.status(200).send({
+    token,
+    email: user.email,
+    firstname: user.firstname,
+    lastname: user.lastname,
+    avatar: user.avatar || '',
+    location: user.location || '',
+    website: user.website || { url: '', title: 'website' },
+    id: user.id
+  })
 })
 
 module.exports = loginRouter
